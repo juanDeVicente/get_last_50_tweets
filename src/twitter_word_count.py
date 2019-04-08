1e8c8b819a83a527da1e3f266d918f76608d8c11
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-04-08 11:42:01 +0200

    Acabada la clase para contar palabras de un tweet.

--- src/twitter_word_count.py
+++ src/twitter_word_count.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-Downloads all tweets from a given user.
-Uses twitter.Api.GetUserTimeline to retreive the last 3,200 tweets from a user.
-Twitter doesn't allow retreiving more tweets than this through the API, so we get
-as many as possible.
-t.py should contain the imported variables.
+Clase que permite contar las palabras de los útlimos tweets
 """
 
-from __future__ import print_function
+from src.word_count import word_count
+from functools import reduce
 
 import twitter
 import os
@@ -20,12 +17,21 @@ CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 
 
-def get_last_n_tweets(api=None, screen_name=None, n_tweets=50):
-    return api.GetUserTimeline(screen_name=screen_name, count=n_tweets)
+class twitter_word_count(object):
+
+    def __init__(self, api):
+        self.api = api
+
+    def get_last_n_tweets(self, screen_name=None):
+        return [tweet.text for tweet in self.api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=False, exclude_replies=True)[0:50]]
+
+    def get_words_of_tweets(self, screen_name=None):
+        tweets = self.get_last_n_tweets(screen_name)
+        tweets = reduce((lambda x, y: x + ', ' + y), tweets)
+        return word_count(tweets, stopwords_language='spanish')
 
 
 if __name__ == "__main__":
-
     try:
         ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
         ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
@@ -43,7 +49,7 @@ if __name__ == "__main__":
     api = twitter.Api(
         CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
     )
-    screen_name = 'perezreverte'  # Aqui va le nombre de la cuenta que queramos mirar (Arturo Perez Reverte) Si es None, devuelve los del usuario a los que este asociado la cuenta
-    tweets = get_last_n_tweets(api=api, screen_name=screen_name, n_tweets=50)
+    screen_name = '_lalusi'  # Aqui va le nombre de la cuenta que queramos mirar (Arturo Perez Reverte) Si es None, devuelve los del usuario a los que este asociado la cuenta
+    twc = twitter_word_count(api)
 
-    print([tweet for tweet in tweets])
+    print(twc.get_words_of_tweets(screen_name))
