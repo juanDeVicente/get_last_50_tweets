7ab2f731d91bd829b8229381cab5bb0f4dd76a06
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-04-08 12:23:37 +0200

    Test de prueba

--- tests/twitter_word_count_test.py
+++ tests/twitter_word_count_test.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pytest
 from unittest.mock import MagicMock
 from src.twitter_word_count import twitter_word_count
 
 
-def basic_test():
+def test_example():
     twc = twitter_word_count(None)
-    twc.get_last_n_tweets = MagicMock(return_value=3)
+    twc.get_last_n_tweets = MagicMock(return_value=['hola', 'que tal estas', 'pues yo muy bien'])
+    assert twc.get_words_of_tweets() == [('hola', 1), ('tal', 1), ('pues', 1), ('bien', 1)]
 
-    assert twc.get_words_of_tweets() == 3
