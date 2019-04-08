44a9dfc35b521f2b7216732c807ef7a676c780d3
ClaudiaRodriguezM <claudiarm2409@hotmail.com>
2019-04-08 15:50:03 +0200

    testeos

--- tests/twitter_word_count_test.py
+++ tests/twitter_word_count_test.py
@@ -1,10 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pytest
 from unittest.mock import MagicMock
 from src.twitter_word_count import twitter_word_count
 
 
-def basic_test():
+def test_example():
+    tweets = ['hola', 'que tal estas', 'pues yo muy bien']
     twc = twitter_word_count(None)
-    twc.get_last_n_tweets = MagicMock(return_value=3)
+    twc.get_last_n_tweets = MagicMock(return_value=tweets)
+    assert twc.get_words_of_tweets() == [('hola', 1), ('tal', 1), ('pues', 1), ('bien', 1)]
 
-    assert twc.get_words_of_tweets() == 3
+
+def test_quijote():
+    tweets = ['En un lugar de la Mancha, de cuyo nombre no quiero acordarme,',
+              'o ha mucho tiempo que vivía un hidalgo de los de lanza en astillero,', 'adarga antigua, rocín flaco',
+              ' y galgo corredor', 'Una olla de algo más vaca que carnero, ',
+              'salpicón las más noches, duelos y quebrantos los sábados,', 'sábados', 'y lentejas los viernes']
+    twc = twitter_word_count(None)
+    twc.get_last_n_tweets = MagicMock(return_value=tweets)
+    print(twc.get_words_of_tweets())
+    assert twc.get_words_of_tweets() == [('sábados', 2), ('lugar', 1), ('mancha', 1), ('cuyo', 1), ('nombre', 1),
+                                         ('quiero', 1), ('acordarme', 1), ('tiempo', 1), ('vivía', 1), ('hidalgo', 1),
+                                         ('lanza', 1), ('astillero', 1), ('adarga', 1), ('antigua', 1), ('rocín', 1),
+                                         ('flaco', 1), ('galgo', 1), ('corredor', 1), ('olla', 1), ('vaca', 1)]
+
+
+def test_none_tweets():
+    tweets = []
+    twc = twitter_word_count(None)
+    twc.get_last_n_tweets = MagicMock(return_value=tweets)
+    with pytest.raises(TypeError):
+        twc.get_words_of_tweets()
