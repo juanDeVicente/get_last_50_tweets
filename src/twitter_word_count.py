5a09637db2ef8f6c7004be84d06fcf19d7ec45d8
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2019-04-08 10:33:09 +0200

    Ahora hay que añadir las variables de entorno para poder ejecutar el porgrama

--- src/twitter_word_count.py
+++ src/twitter_word_count.py
@@ -12,6 +12,7 @@ t.py should contain the imported variables.
 from __future__ import print_function
 
 import twitter
+import os
 
 ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
@@ -24,10 +25,25 @@ def get_last_n_tweets(api=None, screen_name=None, n_tweets=50):
 
 
 if __name__ == "__main__":
+
+    try:
+        ACCESS_TOKEN_KEY = os.environ['ACCESS_TOKEN_KEY']
+        ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+        CONSUMER_KEY = os.environ['CONSUMER_KEY']
+        CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+    except KeyError:
+        print('No se ha establecido algun parametro para conectar con la api de Twitter')
+        exit(-1)
+
+    print(ACCESS_TOKEN_KEY)
+    print(ACCESS_TOKEN_SECRET)
+    print(CONSUMER_KEY)
+    print(CONSUMER_SECRET)
+
     api = twitter.Api(
         CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
     )
-    screen_name = 'perezreverte' #Aqui va le nombre de la cuenta que queramos mirar (Arturo Perez Reverte) Si es None, devuelve los del usuario a los que este asociado la cuenta
+    screen_name = 'perezreverte'  # Aqui va le nombre de la cuenta que queramos mirar (Arturo Perez Reverte) Si es None, devuelve los del usuario a los que este asociado la cuenta
     tweets = get_last_n_tweets(api=api, screen_name=screen_name, n_tweets=50)
 
     print([tweet for tweet in tweets])
