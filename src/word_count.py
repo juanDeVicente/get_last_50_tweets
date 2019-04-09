914021603e4d1569664fa239b3d38a6ef224cb6b
ClaudiaRodriguezM <claudiarm2409@hotmail.com>
2019-04-09 22:40:42 +0200

    testeos

--- src/word_count.py
+++ src/word_count.py
@@ -29,16 +29,21 @@ def word_count(text, stopwords_language='english'):
         .replace("¿", "").replace("¡", "")
     # Los apostrofes son un problema, asi que hay que cambiar los signos de puntuacion a mano
 
+
     words = words.split(' ')  # Separamos por palabras
 
     words = [word.lower() for word in words if word.lower() not in stopwords.words(stopwords_language)]
 
+
     words_count = {}
 
     for key in words:
-        words_count[key] = words_count.get(key, 0) + 1
+        if key != '':
+            words_count[key] = words_count.get(key, 0) + 1
         # get funciona tal que si existe la clave devuelve el valor, si no devuelve el segundo parametro
 
+    if len(words_count) == 0:
+        return []
     return sorted(words_count.items(), key=lambda x: x[1], reverse=True)  # Ordenamos las palabras segun lo pedido
 
 
--- tests/twitter_word_count_test.py
+++ tests/twitter_word_count_test.py
@@ -48,4 +48,4 @@ def test_puntuation_marks():
     tweets = [',,,,', '....', '???', '!!!!', '¡¡¡¡¡', ';;;;', '::::::']
     twc = twitter_word_count(None)
     twc.get_last_n_tweets = MagicMock(return_value=tweets)
-    assert twc.get_words_of_tweets() == [('', 7)]
+    assert twc.get_words_of_tweets() == []
